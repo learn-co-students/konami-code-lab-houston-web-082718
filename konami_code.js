eb35d11dd76cb4bc22fa13367cd65372a4f742b7
gwatson86 <gwatson86@gmail.com>
2018-10-11 10:38:41 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+
+  document.body.addEventListener("keydown", function(x) {
+    if (x.key === codes[index]) {
+      index++;
+    } else {
+      index = 0;
+    }
+
+    if (index === codes.length) {
+      alert("Neeeeeeerrrrrrrddd!");
+      console.log("Test");
+      index = 0;
+    }
+  })
 }
